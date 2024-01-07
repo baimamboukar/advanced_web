4280aff375f7729e92c303771e19a1c7eb7c07ea
baimamboukar <baimamboukar@gmail.com>
2024-01-07 22:49:46 +0100

    chore: add design and enforce google fonts usage

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,8 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Add event listeners to buttons
+  document.getElementById("addButton").addEventListener("click", function () {
+    performOperation("add");
+  });
+
+  document
+    .getElementById("subtractButton")
+    .addEventListener("click", function () {
+      performOperation("subtract");
+    });
+
+  document
+    .getElementById("multiplyButton")
+    .addEventListener("click", function () {
+      performOperation("multiply");
+    });
+
+  document
+    .getElementById("divideButton")
+    .addEventListener("click", function () {
+      performOperation("divide");
+    });
+
+  document
+    .getElementById("clearButton")
+    .addEventListener("click", clearCalculator);
+});
+
 function performOperation(operation) {
   const firstNumber = parseFloat(document.getElementById("firstNumber").value);
   const secondNumber = parseFloat(
     document.getElementById("secondNumber").value
   );
+
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    document.getElementById("warning").innerText = "Please enter valid numbers";
+    return;
+  }
+
   let result;
 
   switch (operation) {
@@ -21,10 +56,12 @@ function performOperation(operation) {
   }
 
   document.getElementById("result").innerText = `Result: ${result}`;
+  document.getElementById("warning").innerText = "";
 }
 
 function clearCalculator() {
   document.getElementById("firstNumber").value = "";
   document.getElementById("secondNumber").value = "";
   document.getElementById("result").innerText = "";
+  document.getElementById("warning").innerText = "";
 }
